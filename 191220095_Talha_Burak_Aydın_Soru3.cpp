ea9bac3dce41ff06f44281eacf8c6025c6b93748
JessFlexx <73108005+JessFlexx@users.noreply.github.com>
2020-12-25 11:56:11 +0300

    20.11.2020-Algoritma Dersi-2.ÖDEV-3.soru
    Girilen sayıların tek veya çift olduğunu kontrol ettikten sonra tek sayıların geometrik ve çift sayıların aritmetik ortalamasını alan koddur.

---
+++ 191220095_Talha_Burak_Aydın_Soru3.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>  //Tek sayilarin geometrik , çift sayýlarýn aaritmetik ortalamasýný alan programdýr. 13.11.2020-Algoritma Dersi-2.ÖDEV-3.soru
+#include <stdlib.h>
+#include <math.h>
+#include <conio.h>
+int main() {
+
+    int tekCarpim,ciftToplam,sayi,z,Scift,Stek;;
+    
+
+    Scift = 0;
+    Stek = 0;
+    tekCarpim = 1;
+    ciftToplam = 0;
+
+    for(z=0; z < 4; z++){
+    printf("Sayi giriniz:");
+    scanf("%d",&sayi);
+
+    if(sayi % 2 == 0){
+        ciftToplam += sayi;
+        Scift++;
+    }
+
+    else{
+        tekCarpim *= sayi;
+        Stek++;
+    }
+    }
+    printf("Cift sayilarin aritmetik ortalamasi: %d\n",ciftToplam/Scift);
+    printf("Tek sayilarin geometrik ortalamasi: %3.4f\n",pow(tekCarpim, (1.0/Stek)));
+
+    getch();
+    return 0;
+}
+
+
