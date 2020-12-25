4ee65d34bb7bd0bb43ea1c857847c1f1590f17db
JessFlexx <73108005+JessFlexx@users.noreply.github.com>
2020-12-25 11:50:11 +0300

    3 sayının ortalaması
    13.11.2020-Algoritma Dersi-1.ÖDEV-1.soru

---
+++ 191220095_Talha_Burak_Aydın_Soru1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h> // 3 sayýnýn ortalamasý. 13.11.2020-Algoritma Dersi-1.ÖDEV-1.soru
+
+int main (){
+	
+	int a, b, c, ort;
+	
+	printf("istediginiz 3 tam sayinin ortalamasini veren programa hosgeldiniz \n1. sayiyi giriniz: ");
+	scanf("%d",&a);
+	
+	printf("2. sayiyi giriniz: ");
+	scanf("%d",&b);
+	
+	printf("3. sayiyi giriniz: ");
+	scanf("%d",&c);
+	
+	
+	
+	ort = (a+b+c)/3 ;
+	
+	printf("girdiniz sayilarin ortalamasi: %d ",ort);
+	
+	
+	return 0;
+}
