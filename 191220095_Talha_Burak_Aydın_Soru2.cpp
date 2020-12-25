ab94dce7a391c51a62bb56982f8b129b564f0384
JessFlexx <73108005+JessFlexx@users.noreply.github.com>
2020-12-25 11:44:58 +0300

    A,B gibi iki sayının karşılaştırılması.
    A,B gibi iki sayının karşılaştırılması.
    13.11.2020-Algoritma Dersi-1.ÖDEV-2.soru

---
+++ 191220095_Talha_Burak_Aydın_Soru2.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h> // A,B gibi iki sayýnýn karþýlaþtýrýlmasý.
+
+int main() 
+
+
+  {
+  	
+  	int a, b;
+	
+	printf("Karsilastirmak istediginiz sayilardan birincisini giriniz : ");
+	scanf("%d",&a);
+	printf("ikinci sayiyi  giriniz: ");
+	scanf("%d",&b);
+	
+	if (a>b) {
+		printf(" %d buyuktur %d sayisindan ",a,b);
+	}
+	else if (a<b){
+		printf("%d kucuktur %d sayisindan " ,a,b);
+	}
+	else if (a=b)
+{ 
+printf("Girdiginiz iki sayi biribirine esittir.");
+
+
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
