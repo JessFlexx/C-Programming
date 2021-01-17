39d1ebc61b3a17bb9f860ad5047f8f74e630cef5
JessFlexx <73108005+JessFlexx@users.noreply.github.com>
2021-01-17 17:12:04 +0300

    Which letters does this text contain?

---
+++ 1.Sınıg-Güz-Ödev-TextContainWhichLetter.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>  // Kullanýcýdan metin girdisi al.Hangi harften kaç tane girildiðini bul.
+#include <stdlib.h>
+
+int main(){
+
+char metin[100],harf;
+int k,i,count=0;
+
+    puts("Bir Metin Giriniz: ");
+    gets(metin);
+    
+ for(k=97;k<123;k++){
+ harf=k;
+        
+ for(i=0;metin[i]!='\0';i++){
+     if(metin[i]==harf) 
+	 count++;
+}
+if(count>0)
+printf("%c karakteri %d kere kullanilmistir.\n",harf,count);
+     
+harf=count=0; //Döngünün kontrol dðeiþkenlerini sýfýrlama
+}
+
+return 0;
+}
